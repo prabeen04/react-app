43e759af14510ef297cb2fb61e7008ecdfc2a761
Prabeen04 <prabeen.strange@gmail.com>
2018-02-10 16:22:02 +0530

    homepage updated

--- src/components/container/home/home.js
+++ src/components/container/home/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
 
 import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
@@ -9,6 +9,9 @@ import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
 import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
 import Image from '../../../assets/images/web.jpg';
+import ResponsiveImage from '../../../assets/images/responsive.png';
+import MacbookImage from '../../../assets/images/macbook.png';
+import IphoneImage from '../../../assets/images/iphone-x.png';
 import BubbleButton from '../../common-components/buttons/bubble-button';
 import './home.css';
 
@@ -82,10 +85,11 @@ class Home extends React.Component {
       </Tabs>
       <Divider /><br/>
       <div className="flex-container">
-        <div className="home-card flex-container">
+        <Link to="/ios"><div className="home-card flex-container">
           <i className="fab fa-apple fa-5x home-icon"></i>
           <h3>IOS Developement</h3>
         </div>
+        </Link>
         <div className="home-card flex-container">
           <i className="fab fa-android fa-5x home-icon"></i>
           <h3>Android Developement</h3>
@@ -105,6 +109,36 @@ class Home extends React.Component {
         <div className="home-card flex-container">
           <i className="fas fa-globe fa-5x home-icon"></i>
           <h3>FrontEnd Developement</h3>
+        </div>
+      </div>
+      <div className="flex-container light-background">
+        <div className="flex-image-container">
+          <img src={IphoneImage}  className="flex-image"/>
+        </div>
+        <div className="flex-description">
+          <h1 className="heading-color">Explore the latest Design Trendz</h1>
+          <p className="text-color">The best and latest App design , Very neat and clear design with intuitive UI/UX design.
+          Mobile is not the future, it is the now. Meet your customers in the environment of their choice, not where it's convenient for you.</p>
+        </div>
+      </div>
+      <div className="flex-container">
+        <div className="flex-description">
+          <h1 className="heading-color">Explore the latest Design Trendz</h1>
+          <p className="text-color">The best and latest App design , Very neat and clear design with intuitive UI/UX design.
+          Mobile is not the future, it is the now. Meet your customers in the environment of their choice, not where it's convenient for you.</p>
+        </div>
+        <div className="flex-image-container">
+          <img src={ResponsiveImage}  className="flex-image"/>
+        </div>
+      </div>
+      <div className="flex-container light-background">
+        <div className="flex-image-container">
+          <img src={MacbookImage}  className="flex-image"/>
+        </div>
+        <div className="flex-description">
+          <h1 className="heading-color">Explore the latest Design Trendz</h1>
+          <p className="text-color">The best and latest App design , Very neat and clear design with intuitive UI/UX design.
+          Mobile is not the future, it is the now. Meet your customers in the environment of their choice, not where it's convenient for you.</p>
         </div>
       </div>
     </div>);
