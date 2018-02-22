8c83275da92ee2814af8c3866d555976ba72f9b2
Prabeen04 <prabeen.strange@gmail.com>
2018-02-22 20:27:40 +0530

    nprogress css file styles changed

--- src/components/container/chat.js
+++ src/components/container/chat.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import nprogress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 class Chat extends React.Component{
-
+  constructor(props){
+    super(props)
+    nprogress.start();
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      console.log('its done')
+      nprogress.done();
+    },2000)
+  }
   render(){
     return(
       <h1>Chat Component</h1>
