5792bc49be89b65401346b459a41cb3254271eae
Prabeen04 <prabeen.strange@gmail.com>
2018-02-26 21:37:34 +0530

    hover effect  button hover added

--- src/components/common-components/buttons/hover-button.js
+++ src/components/common-components/buttons/hover-button.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const onmousemove = (e) => {
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+    console.log(x, y)
+    e.target.style.setProperty('--x', `${ x }px`)
+    e.target.style.setProperty('--y', `${ y }px`)
+
+}
 const HoverButton = () => {
+
     return(
-        <button className="hover-button"><span>Hover Over Me</span></button>
+        <button className="hover-button" onMouseMove={onmousemove}><span>Hover Over Me</span></button>
     );
 }
 export default HoverButton;
