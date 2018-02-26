17ceb5347396d4f7fd7b053da435a72ed61d24e1
Prabeen04 <prabeen.strange@gmail.com>
2018-02-26 21:28:48 +0530

    hover button added

---
+++ src/components/common-components/buttons/hover-button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const HoverButton = () => {
+    return(
+        <button className="hover-button"><span>Hover Over Me</span></button>
+    );
+}
+export default HoverButton;
--- src/components/container/about/about.js
+++ src/components/container/about/about.js
@@ -6,11 +6,15 @@ import HorizontalLinearStepper from '../../common-components/stepper/horizontal-
 import LineChart from '../../utility/chart-js/line-chart';
 import PieChart from '../../utility/chart-js/pie-chart';
 import BubbleChart from '../../utility/chart-js/bubble-chart';
+import HoverButton from '../../common-components/buttons/hover-button'
 import Paper from 'material-ui/Paper';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import FontIcon from 'material-ui/FontIcon';
 import MapsPersonPin from 'material-ui/svg-icons/maps/person-pin';
-
+import Dashboard from 'material-ui/svg-icons/action/dashboard';
+import Devices from 'material-ui/svg-icons/device/devices';
+import Forum from 'material-ui/svg-icons/communication/forum';
+import AccountCircle from 'material-ui/svg-icons/action/account-circle';
 
 class About extends React.Component {
   constructor(props) {
@@ -29,13 +33,14 @@ class About extends React.Component {
   render() {
     return (
       <div>
-        <Tabs value={this.state.value} onChange={this.handleChange} inkBarStyle={{ background: 'tomato' }}>
-          <Tab style={{ backgroundColor: '#555', selectedBackgroundColor: 'red' }} icon={<FontIcon className="material-icons" > phone</FontIcon>} label="RECENTS" value="a">
+        <HoverButton />
+        <Tabs value={this.state.value} onChange={this.handleChange}>
+          <Tab style={{ backgroundColor: '#555'}} icon={<Dashboard />} label="RECENTS" value="a">
             <div className="flex-container">
               <PieChart />
             </div>
           </Tab>
-          <Tab style={{ backgroundColor: '#666' }} icon={<FontIcon className="material-icons" > favorite</FontIcon>} label="FAVORITES" value="b">
+          <Tab style={{ backgroundColor: '#666' }} icon={<Devices />} label="FAVORITES" value="b">
             <div className="flex-container">
               <BubbleChart />
             </div>
