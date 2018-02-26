022445acb508b9101fa11f28bca5f81a47a8cb46
Prabeen04 <prabeen.strange@gmail.com>
2018-02-26 20:05:20 +0530

    notfound and gallary component were modified to stateless component

--- src/components/container/gallary.js
+++ src/components/container/gallary.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import SimpleGallery from '../common-components/gallery/simple-gallery';
-class Gallary extends React.Component{
 
-  render(){
-    return(
-      <SimpleGallery />
-    );
-  }
-
-}
-
-export default Gallary;
+const Gallary = ()=>  <SimpleGallery /> 
+export default  Gallary;
--- src/components/container/notfound.js
+++ src/components/container/notfound.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-class NotFound extends React.Component{
+ const NotFound = () =>  <h1 className="flexbox-container">404!!! Page You are looking for is NOT FOUND</h1>
 
-  render(){
-    return(
-      <h1  className="flexbox-container">404!!! Page You are looking for is NOT FOUND</h1>
-    );
-  }
-
-}
-
-export default NotFound;
+ export default NotFound;
